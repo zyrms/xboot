239f7ac8b6272e1edaf353d213b9d595df8e55fd
jerryjianjun <jerryjianjun@gmail.com>
2012-10-19 15:10:08 +0800

    portting qrencode and write and test command. done!!

--- src/kernel/command/cmd_test.c
+++ src/kernel/command/cmd_test.c
@@ -45,25 +45,141 @@
 
 #if	defined(CONFIG_COMMAND_TEST) && (CONFIG_COMMAND_TEST > 0)
 
+#include <qrencode.h>
+
+static int casesensitive = 1;
+static int eightbit = 0;
+static int version = 1;
+static int size = 4;
+static int margin = 4;
+static int structured = 0;
+static int micro = 0;
+static QRecLevel level = QR_ECLEVEL_L;
+static QRencodeMode hint = QR_MODE_8;
+
+static void draw_QRcode(QRcode *qrcode, struct surface_t * screen, int ox, int oy)
+{
+	int x, y, width;
+	unsigned char *p;
+	struct rect_t rect;
+	int size = 4;
+
+	ox += margin * size;
+	oy += margin * size;
+	width = qrcode->width;
+	p = qrcode->data;
+	for (y = 0; y < width; y++)
+	{
+		for (x = 0; x < width; x++)
+		{
+			rect.x = ox + x * size;
+			rect.y = oy + y * size;
+			rect.w = size;
+			rect.h = size;
+
+			surface_fill(screen, &rect, (*p & 1) ? 0 : 0xffffff, BLEND_MODE_REPLACE);
+			p++;
+		}
+	}
+}
+
+void draw_singleQRcode(QRinput *stream, int mask, struct surface_t * screen)
+{
+	u32_t c;
+	QRcode *qrcode;
+	int width;
+
+	QRinput_setVersionAndErrorCorrectionLevel(stream, version, level);
+	if(micro) {
+		qrcode = QRcode_encodeMaskMQR(stream, mask);
+	} else {
+		qrcode = QRcode_encodeMask(stream, mask);
+	}
+	if(qrcode == NULL) {
+		width = (11 + margin * 2) * size;
+		fprintf(stderr, "Input data does not fit to this setting.\n");
+	} else {
+		version = qrcode->version;
+		width = (qrcode->width + margin * 2) * size;
+	}
+
+	c = surface_map_color(screen, get_color_by_name("white"));
+	surface_fill(screen, &screen->clip, c, BLEND_MODE_REPLACE);
+	if(qrcode) {
+		draw_QRcode(qrcode, screen, 0, 0);
+	}
+	QRcode_free(qrcode);
+}
+
+void view_simple(unsigned char *str, int length, struct surface_t * screen)
+{
+	QRinput *input;
+	int ret;
+
+	if(micro) {
+		input = QRinput_newMQR(version, level);
+	} else {
+		input = QRinput_new2(version, level);
+	}
+	if(input == NULL) {
+		fprintf(stderr, "Memory allocation error.\n");
+	}
+	if(eightbit) {
+		ret = QRinput_append(input, QR_MODE_8, length, str);
+	} else {
+		ret = Split_splitStringToQRinput((char *)str, input, hint, casesensitive);
+	}
+	if(ret < 0) {
+	}
+
+	draw_singleQRcode(input, -1, screen);
+
+	QRinput_free(input);
+}
+
 static int test(int argc, char ** argv)
 {
-	extern void test_elf(char * path);
-	if(argc >= 2)
-	{
-		test_elf(argv[1]);
-	}
-	else
-	{
-		void (*p)(void);
+	char buf[256];
+	static int i = 0;
 
-		p = __symbol_get("module_main");
-		if(p)
+	struct fb * fb;
+	struct surface_t * screen;
+
+	fb = search_framebuffer("fb");
+	screen = &fb->info->surface;
+
+	struct console * con = get_console_stdout();
+	console_setcursor(con, FALSE);
+	console_cls(con);
+	u32_t code;
+
+	while(1)
+	{
+		if(console_stdin_getcode_with_timeout(&code, 250))
 		{
-			p();
-		}
-		else
-		{
-			printk("not found module_main\r\n");
+			switch(code)
+			{
+			case 0x10:	/* up */
+
+				break;
+
+			case 0xe:	/* down */
+				sprintf(buf, "mytest qrencode: val = %d", i++);
+				view_simple(buf, strlen(buf), screen);
+				break;
+
+			case 0x2:	/* left */
+
+				break;
+
+			case 0x6:	/* right */
+
+				break;
+
+			default:
+
+				break;
+			}
 		}
 	}
 
